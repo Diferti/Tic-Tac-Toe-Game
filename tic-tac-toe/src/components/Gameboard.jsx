cfea2678821ad7b67b3ee219cbb0c0d592a60cf5
Dmitrii Semenov <dima.cemenov70@yandex.ru>
2024-08-21 14:09:53 -0400

    Added game logic

--- tic-tac-toe/src/components/Gameboard.jsx
+++ tic-tac-toe/src/components/Gameboard.jsx
@@ -3,15 +3,14 @@ import '../css/App.css'
 import xImage from '../assets/x.jpg';
 import oImage from '../assets/o.jpg';
 
-function Field({value, slot, step}) {
-    const [image, setImage] = useState(null);
+function Field({slot, step, winner}) {
     const [isFlipped, setIsFlipped] = useState(false);
 
     function click() {
-        console.log(`Field №${value} was clicked!`);
-        setImage(xImage);
-        setIsFlipped(true);
-        step();
+        if (!winner) {
+            setIsFlipped(true);
+            step();
+        }
     }
 
     return (
@@ -19,7 +18,7 @@ function Field({value, slot, step}) {
             <div className="field-container">
                 <div className="field-front"></div>
                 <div className="field-back">
-                    <img className="field-icon" src={image} alt=""/>
+                    <img className="field-icon" src={slot === "X" ? xImage : oImage} alt=""/>
                 </div>
             </div>
         </button>
@@ -28,32 +27,65 @@ function Field({value, slot, step}) {
 
 export default function Gameboard() {
     const [result, setResult] = useState(Array(9).fill(null));
+    const [xMove, setXMove] = useState(true);
 
     function stepClick(index) {
+        if (result[index] || winnerCalculation(result)) { return; }
         const step = result.slice();
-        step[index] = "X";
+        step[index] = xMove ? "X" : "O";
         setResult(step);
+        setXMove(!xMove);
+    }
+
+    const winner = winnerCalculation(result);
+    let status;
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (xMove ? 'X' : 'O');
     }
 
     return (
         <>
+            <div className="status">{status}</div>
             <div className="board">
                 <div className="row">
-                    <Field value="1" slot={result[0]} step={() => stepClick(0)}/>
-                    <Field value="2" slot={result[1]} step={() => stepClick(1)}/>
-                    <Field value="3" slot={result[2]} step={() => stepClick(2)}/>
+                    <Field slot={result[0]} step={() => stepClick(0)} winner={winner} />
+                    <Field slot={result[1]} step={() => stepClick(1)} winner={winner} />
+                    <Field slot={result[2]} step={() => stepClick(2)} winner={winner}/>
                 </div>
                 <div className="row">
-                    <Field value="4" slot={result[3]} step={() => stepClick(3)}/>
-                    <Field value="5" slot={result[4]} step={() => stepClick(4)}/>
-                    <Field value="6" slot={result[5]} step={() => stepClick(5)}/>
+                    <Field slot={result[3]} step={() => stepClick(3)} winner={winner}/>
+                    <Field slot={result[4]} step={() => stepClick(4)} winner={winner}/>
+                    <Field slot={result[5]} step={() => stepClick(5)} winner={winner}/>
                 </div>
                 <div className="row">
-                    <Field value="7" slot={result[6]} step={() => stepClick(6)}/>
-                    <Field value="8" slot={result[7]} step={() => stepClick(7)}/>
-                    <Field value="9" slot={result[8]} step={() => stepClick(8)}/>
+                    <Field slot={result[6]} step={() => stepClick(6)} winner={winner}/>
+                    <Field slot={result[7]} step={() => stepClick(7)} winner={winner}/>
+                    <Field slot={result[8]} step={() => stepClick(8)} winner={winner}/>
                 </div>
             </div>
         </>
     );
+}
+
+function winnerCalculation(result) {
+    const victoryOptions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i=0; i<victoryOptions.length; i++) {
+        let [a, b, c] = victoryOptions[i];
+        if (result[a] && result[a] === result[b] && result[a] === result[c]) {
+            return result[a];
+        }
+    }
+    return null;
 }
