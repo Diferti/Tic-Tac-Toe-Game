a9e61a948da39683ac86105ae60241af1688a5e4
Dmitrii Semenov <dima.cemenov70@yandex.ru>
2024-08-21 18:34:52 -0400

    Added step and win animations

--- tic-tac-toe/src/components/Gameboard.jsx
+++ tic-tac-toe/src/components/Gameboard.jsx
@@ -3,7 +3,7 @@ import '../css/App.css'
 import xImage from '../assets/x.jpg';
 import oImage from '../assets/o.jpg';
 
-function Field({slot, step, winner}) {
+function Field({index, slot, step, winner}) {
     const [isFlipped, setIsFlipped] = useState(false);
 
     function click() {
@@ -13,9 +13,12 @@ function Field({slot, step, winner}) {
         }
     }
 
+    const winnerLine = winner && winner.line.includes(index);
+    const fieldColor = slot === "X" ? 'xColor' : 'oColor';
+
     return (
         <button className={`field ${isFlipped ? 'flipped' : ''}`} onClick={click}>
-            <div className="field-container">
+            <div className={`field-container ${fieldColor} ${winnerLine ? 'winner-line' : ''}`}>
                 <div className="field-front"></div>
                 <div className="field-back">
                     <img className="field-icon" src={slot === "X" ? xImage : oImage} alt=""/>
@@ -38,33 +41,33 @@ export default function Gameboard() {
     }
 
     const winner = winnerCalculation(result);
-    let status;
-    if (winner) {
-        status = 'Winner: ' + winner;
-    } else {
-        status = 'Next player: ' + (xMove ? 'X' : 'O');
-    }
 
     return (
         <>
-            <div className="status">{status}</div>
             <div className="board">
                 <div className="row">
-                    <Field slot={result[0]} step={() => stepClick(0)} winner={winner} />
-                    <Field slot={result[1]} step={() => stepClick(1)} winner={winner} />
-                    <Field slot={result[2]} step={() => stepClick(2)} winner={winner}/>
+                    <Field index={0} slot={result[0]} step={() => stepClick(0)} winner={winner} />
+                    <Field index={1} slot={result[1]} step={() => stepClick(1)} winner={winner} />
+                    <Field index={2} slot={result[2]} step={() => stepClick(2)} winner={winner}/>
                 </div>
                 <div className="row">
-                    <Field slot={result[3]} step={() => stepClick(3)} winner={winner}/>
-                    <Field slot={result[4]} step={() => stepClick(4)} winner={winner}/>
-                    <Field slot={result[5]} step={() => stepClick(5)} winner={winner}/>
+                    <Field index={3} slot={result[3]} step={() => stepClick(3)} winner={winner}/>
+                    <Field index={4} slot={result[4]} step={() => stepClick(4)} winner={winner}/>
+                    <Field index={5} slot={result[5]} step={() => stepClick(5)} winner={winner}/>
                 </div>
                 <div className="row">
-                    <Field slot={result[6]} step={() => stepClick(6)} winner={winner}/>
-                    <Field slot={result[7]} step={() => stepClick(7)} winner={winner}/>
-                    <Field slot={result[8]} step={() => stepClick(8)} winner={winner}/>
+                    <Field index={6} slot={result[6]} step={() => stepClick(6)} winner={winner}/>
+                    <Field index={7} slot={result[7]} step={() => stepClick(7)} winner={winner}/>
+                    <Field index={8} slot={result[8]} step={() => stepClick(8)} winner={winner}/>
                 </div>
             </div>
+            {winner && (
+                <div className="winner">
+                    <h1 className="winner-title">WINNER {winner.player === 'X' ? 'BEAVER' : 'CAPYBARA'}</h1>
+                    <h3 className="winner-subtitle">(PLAYER {winner.player})</h3>
+                    <img className="winner-image" src={winner.player === 'X' ? xImage : oImage} alt=""/>
+                </div>
+            )}
         </>
     );
 }
@@ -84,7 +87,10 @@ function winnerCalculation(result) {
     for (let i=0; i<victoryOptions.length; i++) {
         let [a, b, c] = victoryOptions[i];
         if (result[a] && result[a] === result[b] && result[a] === result[c]) {
-            return result[a];
+            return {
+                player: result[a],
+                line: [a, b, c]
+            };
         }
     }
     return null;
